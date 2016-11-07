c9c7510fbdf2fd7c93c1f90b59db8baed2063658
Arel Cordero <arel@ditto.us.com>
2016-11-07 17:29:12 -0500

    open cloud9 on localhost:8000

--- main.js
+++ main.js
@@ -22,8 +22,10 @@ function createWindow () {
     slashes: true
   }))
 
+  mainWindow.maximize()
+
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
